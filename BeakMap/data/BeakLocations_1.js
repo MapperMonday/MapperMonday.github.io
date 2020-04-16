190aed275f7a9ba651f7caa1565333d5ec422a24
Andrew Helm <63794666+MapperMonday@users.noreply.github.com>
2020-04-16 14:37:24 +0100

    Beak Ma0 v1.0

---
+++ BeakMap/data/BeakLocations_1.js
@@ -0,0 +1,26 @@
+var json_BeakLocations_1 = {
+"type": "FeatureCollection",
+"name": "BeakLocations_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Pill", "Release": "Recordings 05\/01\/09 - 17\/01\/09" }, "geometry": { "type": "Point", "coordinates": [ -2.687754461752278, 51.480209910764273 ] } },
+{ "type": "Feature", "properties": { "Name": "Backwell", "Release": "Recordings 05\/01\/09 - 17\/01\/09" }, "geometry": { "type": "Point", "coordinates": [ -2.741360400640844, 51.411628646933202 ] } },
+{ "type": "Feature", "properties": { "Name": "Ham Green", "Release": "Recordings 05\/01\/09 - 17\/01\/09" }, "geometry": { "type": "Point", "coordinates": [ -2.679838235893154, 51.47542189243449 ] } },
+{ "type": "Feature", "properties": { "Name": "Battery Point", "Release": "Recordings 05\/01\/09 - 17\/01\/09" }, "geometry": { "type": "Point", "coordinates": [ -2.773113608434046, 51.494538610575397 ] } },
+{ "type": "Feature", "properties": { "Name": "Iron Acton", "Release": "Recordings 05\/01\/09 - 17\/01\/09" }, "geometry": { "type": "Point", "coordinates": [ -2.462219940375354, 51.549334220817698 ] } },
+{ "type": "Feature", "properties": { "Name": "Blagdon Lake", "Release": "Recordings 05\/01\/09 - 17\/01\/09" }, "geometry": { "type": "Point", "coordinates": [ -2.697815964041566, 51.33429964512591 ] } },
+{ "type": "Feature", "properties": { "Name": "Barrow Gurney", "Release": "Recordings 05\/01\/09 - 17\/01\/09" }, "geometry": { "type": "Point", "coordinates": [ -2.677017690852584, 51.40841461275695 ] } },
+{ "type": "Feature", "properties": { "Name": "The Cornubia", "Release": "Recordings 05\/01\/09 - 17\/01\/09" }, "geometry": { "type": "Point", "coordinates": [ -2.587814707467369, 51.452406511935749 ] } },
+{ "type": "Feature", "properties": { "Name": "Dundry Hill", "Release": "Recordings 05\/01\/09 - 17\/01\/09" }, "geometry": { "type": "Point", "coordinates": [ -2.643415285942612, 51.397597987711983 ] } },
+{ "type": "Feature", "properties": { "Name": "Flax Bourton", "Release": "Recordings 05\/01\/09 - 17\/01\/09" }, "geometry": { "type": "Point", "coordinates": [ -2.709186448559721, 51.420835344861942 ] } },
+{ "type": "Feature", "properties": { "Name": "Yatton", "Release": ">>" }, "geometry": { "type": "Point", "coordinates": [ -2.819213677003535, 51.386616723966839 ] } },
+{ "type": "Feature", "properties": { "Name": "Ladies Mile", "Release": ">>" }, "geometry": { "type": "Point", "coordinates": [ -2.623531222564135, 51.468789009738778 ] } },
+{ "type": "Feature", "properties": { "Name": "Failand", "Release": ">> (Bonus Album)" }, "geometry": { "type": "Point", "coordinates": [ -2.692190457082611, 51.440525361747817 ] } },
+{ "type": "Feature", "properties": { "Name": "Dursley", "Release": ">> (Bonus Album)" }, "geometry": { "type": "Point", "coordinates": [ -2.356083298000113, 51.682237955806507 ] } },
+{ "type": "Feature", "properties": { "Name": "Lulsgate", "Release": ">> (Bonus Album)" }, "geometry": { "type": "Point", "coordinates": [ -2.700879884268347, 51.387527585512395 ] } },
+{ "type": "Feature", "properties": { "Name": "Nailsea", "Release": ">> (Bonus Album)" }, "geometry": { "type": "Point", "coordinates": [ -2.762981573521118, 51.433577664597806 ] } },
+{ "type": "Feature", "properties": { "Name": "Clutton", "Release": ">> (Bonus Album)" }, "geometry": { "type": "Point", "coordinates": [ -2.540784843265227, 51.330829357973471 ] } },
+{ "type": "Feature", "properties": { "Name": "Brean Down", "Release": ">>>" }, "geometry": { "type": "Point", "coordinates": [ -3.02740255705641, 51.325581442295146 ] } },
+{ "type": "Feature", "properties": { "Name": "Abbots Leigh", "Release": ">>>" }, "geometry": { "type": "Point", "coordinates": [ -2.661165153420766, 51.460733104401641 ] } }
+]
+}
