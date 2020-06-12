49f85f1d8e91cce08fbf7e495ebeafe66769f58b
Andrew Helm <63794666+MapperMonday@users.noreply.github.com>
2020-06-12 11:27:14 +0100

    Furloughed Jobs in England

---
+++ furlough/data/Cities_1.js
@@ -0,0 +1,25 @@
+var json_Cities_1 = {
+"type": "FeatureCollection",
+"name": "Cities_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Name": "Birmingham" }, "geometry": { "type": "Point", "coordinates": [ -1.898430412182223, 52.478995423688275 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Name": "Sheffield" }, "geometry": { "type": "Point", "coordinates": [ -1.464305475910998, 53.379993676707301 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Name": "Leeds" }, "geometry": { "type": "Point", "coordinates": [ -1.547622718977543, 53.795345987724382 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Name": "Manchester" }, "geometry": { "type": "Point", "coordinates": [ -2.245459832218674, 53.480571479669287 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Name": "Liverpool" }, "geometry": { "type": "Point", "coordinates": [ -2.989823716175485, 53.407158067510601 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Name": "Newcastle Upon Tyne" }, "geometry": { "type": "Point", "coordinates": [ -1.616358806247342, 54.969798319982253 ] } },
+{ "type": "Feature", "properties": { "id": "7", "Name": "Bristol" }, "geometry": { "type": "Point", "coordinates": [ -2.59693019337996, 51.454171255766916 ] } },
+{ "type": "Feature", "properties": { "id": "8", "Name": "London" }, "geometry": { "type": "Point", "coordinates": [ -0.126175722382645, 51.500827626872145 ] } },
+{ "type": "Feature", "properties": { "id": "9", "Name": "Cambridge" }, "geometry": { "type": "Point", "coordinates": [ 0.124503375935788, 52.206694742045656 ] } },
+{ "type": "Feature", "properties": { "id": "10", "Name": "Norwich" }, "geometry": { "type": "Point", "coordinates": [ 1.299348678522314, 52.629209805385862 ] } },
+{ "type": "Feature", "properties": { "id": "11", "Name": "Brighton" }, "geometry": { "type": "Point", "coordinates": [ -0.143587638633591, 50.823528664218927 ] } },
+{ "type": "Feature", "properties": { "id": "12", "Name": "Southampton" }, "geometry": { "type": "Point", "coordinates": [ -1.403198093822586, 50.904522748392417 ] } },
+{ "type": "Feature", "properties": { "id": "13", "Name": "Exeter" }, "geometry": { "type": "Point", "coordinates": [ -3.531799513693366, 50.723962621797803 ] } },
+{ "type": "Feature", "properties": { "id": "14", "Name": "Oxford" }, "geometry": { "type": "Point", "coordinates": [ -1.26208823036396, 51.750785666450916 ] } },
+{ "type": "Feature", "properties": { "id": "15", "Name": "Edinburgh" }, "geometry": null },
+{ "type": "Feature", "properties": { "id": "16", "Name": "Glasgow" }, "geometry": null },
+{ "type": "Feature", "properties": { "id": "17", "Name": "Nottingham" }, "geometry": { "type": "Point", "coordinates": [ -1.14790505192689, 52.949040617212134 ] } },
+{ "type": "Feature", "properties": { "id": "18", "Name": "Cardiff" }, "geometry": null }
+]
+}
