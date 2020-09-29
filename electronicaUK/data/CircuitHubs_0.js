9287052990d1367eeaf19232d9b45f0b6ba74dd3
Andrew Helm <63794666+MapperMonday@users.noreply.github.com>
2020-09-29 10:54:23 +0100

    UK Electronica v3

--- electronicaUK/data/CircuitHubs_0.js
+++
@@ -1,9 +0,0 @@
-var json_CircuitHubs_0 = {
-"type": "FeatureCollection",
-"name": "CircuitHubs_0",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "id": "1", "Name": "London" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -0.197455532551, 51.57236500789493 ], [ -0.01763135724394, 51.569604897870491 ], [ -0.02238576162024, 51.457376670839295 ], [ -0.201931529403293, 51.460267910316666 ], [ -0.197455532551, 51.57236500789493 ] ] ] ] } },
-{ "type": "Feature", "properties": { "id": "2", "Name": "Brighton" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -0.22320873384623, 50.89861525010992 ], [ -0.046615192973475, 50.895513488384374 ], [ -0.051285002252975, 50.783552098696909 ], [ -0.227909107047347, 50.78636947414379 ], [ -0.22320873384623, 50.89861525010992 ] ] ] ] } }
-]
-}
