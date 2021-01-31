023508045f06c6195aec6729b1fd15f377d0763e
Andrew Helm <63794666+MapperMonday@users.noreply.github.com>
2021-01-31 18:00:53 +0000

    Indie HSL
    New categories added

---
+++ IndieHSL/data/AntiquesCollectibles_8.js
@@ -0,0 +1,19 @@
+var json_AntiquesCollectibles_8 = {
+"type": "FeatureCollection",
+"name": "AntiquesCollectibles_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Roberts Rummage", "Address": "68 High Street", "Phone": null, "Website": null }, "geometry": { "type": "Point", "coordinates": [ 0.591194958524814, 50.857172082732838 ] } },
+{ "type": "Feature", "properties": { "Name": "Courthouse Street Yard", "Address": "Courthouse Street", "Phone": null, "Website": null }, "geometry": { "type": "Point", "coordinates": [ 0.592097396936661, 50.857119335535735 ] } },
+{ "type": "Feature", "properties": { "Name": "Reeves & Son", "Address": "31 Courthouse Street", "Phone": "01424 437672", "Website": null }, "geometry": { "type": "Point", "coordinates": [ 0.591740878057906, 50.857312741633407 ] } },
+{ "type": "Feature", "properties": { "Name": "Goods Depot", "Address": "86 High Street", "Phone": "01424 532662", "Website": "goodsdepot.co.uk" }, "geometry": { "type": "Point", "coordinates": [ 0.592208809086271, 50.858132071295906 ] } },
+{ "type": "Feature", "properties": { "Name": "Clockwork Crow", "Address": "68 George Street", "Phone": "01424 716847", "Website": "theclockworkcrow.co.uk" }, "geometry": { "type": "Point", "coordinates": [ 0.589714569586861, 50.856279767949843 ] } },
+{ "type": "Feature", "properties": { "Name": "Hastings Antiques Warehouse", "Address": "54 High Street", "Phone": "01424 433142", "Website": "hastingsantiques.co.uk" }, "geometry": { "type": "Point", "coordinates": [ 0.591107221456995, 50.856921533015274 ] } },
+{ "type": "Feature", "properties": { "Name": "Kings Road Bazaar", "Address": "10-11 Kings Road", "Phone": "01424 721226", "Website": "facebook.com\/BazaarKingsRoad10" }, "geometry": { "type": "Point", "coordinates": [ 0.560349112253211, 50.854612895783063 ] } },
+{ "type": "Feature", "properties": { "Name": "Calneva", "Address": "13 Kings Road", "Phone": "01424 552208", "Website": "instagram.com\/calneva_vintage" }, "geometry": { "type": "Point", "coordinates": [ 0.560326829823289, 50.854398378371471 ] } },
+{ "type": "Feature", "properties": { "Name": "Grand Designs Antiques", "Address": "24 Grand Parade", "Phone": "01424 436876", "Website": "granddesignsshop.co.uk" }, "geometry": { "type": "Point", "coordinates": [ 0.561970159030048, 50.851929598782981 ] } },
+{ "type": "Feature", "properties": { "Name": "Antiques-on-Sea", "Address": "17-18 Tower Road", "Phone": "07450 025086", "Website": "instagram.com\/antiques_on_sea" }, "geometry": { "type": "Point", "coordinates": [ 0.558764274424999, 50.862712843461011 ] } },
+{ "type": "Feature", "properties": { "Name": "Style 1900", "Address": "27 Grand Parade", "Phone": "01424 445453", "Website": "facebook.com\/style1900decorativearts" }, "geometry": { "type": "Point", "coordinates": [ 0.560307332697107, 50.85160076998757 ] } },
+{ "type": "Feature", "properties": { "Name": "Teddy Tinker's", "Address": "134 London Road", "Phone": "07516 268667", "Website": "facebook.com\/TeddyTinkers" }, "geometry": { "type": "Point", "coordinates": [ 0.556920403348941, 50.861721303998948 ] } }
+]
+}
---
+++ IndieHSL/data/Artisans_9.js
@@ -0,0 +1,9 @@
+var json_Artisans_9 = {
+"type": "FeatureCollection",
+"name": "Artisans_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "Wood Recycling Project", "Address": "42 Brittania Enterprise Centre", "Phone": "01424 715566", "Website": null }, "geometry": { "type": "Point", "coordinates": [ 0.583315013392486, 50.861111183553561 ] } },
+{ "type": "Feature", "properties": { "Name": "Coastal Cocoa", "Address": null, "Phone": "01424 563046", "Website": "coastalcocoa.co.uk" }, "geometry": { "type": "Point", "coordinates": [ 0.603721541245267, 50.864729193198059 ] } }
+]
+}
